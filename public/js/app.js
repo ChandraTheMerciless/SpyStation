ca1528721c07c22c3f29f1c727aae370120e4a55
Chandra <chandra.asar@gmail.com>
2016-08-16 23:27:35 -0400

    javascript functions

--- public/js/app.js
+++ public/js/app.js
@@ -13,70 +13,65 @@ var AppModule = function () {
             var code = Cypher.encodeAlph(DomObj.userInput.value);
 
 
-            code = code.toString();
+            var codeStr = code.toString();
 
-            //put into separate functions
+            //DomObj.ringTone.play();
 
-            var paraCode = document.createElement("p");
-            var nodeCode = document.createTextNode("Code code code");
-            paraCode.appendChild(nodeCode);
-
-            var element = document.getElementById("console-container");
-            var msgInput = document.getElementById("msg-start");
-            element.insertBefore(paraCode,msgInput);
-
-            var paraInstruc = document.createElement("p");
-            var nodeInstruc = document.createTextNode("What's your message?");
-            paraInstruc.appendChild(nodeInstruc);
-
-            element.insertBefore(paraInstruc,msgInput);
-            //into separate function end
-
-            DomObj.ringTone.play();
-
-            DomObj.ringTone.addEventListener('ended', function () {
-                responsiveVoice.speak(code, "UK English Male",
-                    {
-                        //rate: 0.8,
-                        //pitch: 1
-                    });
-            });
+            //DomObj.ringTone.addEventListener('ended', function () {
+            responsiveVoice.speak(codeStr, "UK English Male",
+                {
+                    //rate: 0.8,
+                    //pitch: 1
+                });
+            //});
 
             //send this to server with url 'statuses/update'
             var tweet = {
-                status: "#secretcode " + code
+                status: "#secretcode " + codeStr
             };
 
-
-            var xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == XMLHttpRequest.DONE) {
-                    if (xhr.status == 200) {
-                        console.log(xhr.responseText);
-                    }
-                    else if (xhr.status == 400) {
-                        console.log('There was an error 400');
-                    }
-                    else {
-                        console.log('something else other than 200 was returned');
-                    }
-                }
-            };
-
-
-            xhr.open('POST', '/api/code', true);
-            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-            xhr.send(JSON.stringify(tweet));
-
-
-            //responsiveVoice.speak(code, "UK English Male",
-            //    {
-            //        //rate: 0.8,
-            //        //pitch: 1
-            //    });
-            //var msg = new SpeechSynthesisUtterance('Hello World');//window.speechSynthesis.speak(msg);
+            pushTextToConsole();
+            var test = sendAjaxReq(tweet);
+            console.log(test);
         }
+    };
+
+    function pushTextToConsole() {
+        var element = document.getElementById("console-container");
+        var msgInput = document.getElementById("msg-start");
+
+        var paraConfirm = document.createElement("p");
+        var nodeConfirm = document.createTextNode("Message sent.");
+        paraConfirm.appendChild(nodeConfirm);
+        element.insertBefore(paraConfirm, msgInput);
+
+        var paraInstruc = document.createElement("p");
+        var nodeInstruc = document.createTextNode("What's your message?");
+        paraInstruc.appendChild(nodeInstruc);
+        element.insertBefore(paraInstruc, msgInput);
+    };
+
+    function sendAjaxReq(tweet) {
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                if (xhr.status == 200) {
+                    console.log(xhr.responseText);
+                }
+                else if (xhr.status == 400) {
+                    console.log('There was an error 400');
+                }
+                else {
+                    console.log('something else other than 200 was returned');
+                }
+            }
+        };
+
+
+        xhr.open('POST', '/api/code', true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.send(JSON.stringify(tweet));
     };
 
     return {
